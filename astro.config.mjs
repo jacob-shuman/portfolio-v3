7a2a8b1b9c734216440af3155818d2d1e8e26674
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-06-11 08:08:51 -0400

    fix: readd sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
@@ -6,7 +7,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacobshuman.ca",
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), sitemap()],
   output: "server",
   adapter: cloudflare(),
   vite: { ssr: { external: ["svgo"] } },
