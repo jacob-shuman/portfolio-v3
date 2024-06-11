eec8dda73e85745fde8b942ad1a91ec38ff35b3e
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-06-11 08:25:24 -0400

    fix: astro icon hopefully

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacobshuman.ca",
-  integrations: [tailwind(), svelte(), sitemap()],
+  integrations: [tailwind(), svelte(), sitemap(), icon()],
   output: "server",
   adapter: cloudflare(),
-  vite: { ssr: { external: ["svgo"] } },
+  // vite: { ssr: { external: ["svgo"] } },
 });
